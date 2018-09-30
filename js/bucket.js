b804c3d70b427722b6ad3af110ad2c0f5add8674
Tanvimaha <43727579+Tanvimaha@users.noreply.github.com>
2018-09-30 23:00:57 +0530

    Initial commit

---
+++ js/bucket.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+$("#button-toggle").click(function(){
+ $("#band-info").toggle(1000);
+
+
+
+ });
+
+});
+
+$( function() {
+   $( "#accordion" ).accordion();
+ } );
